d5c431e553702b040779f36b7ba3b34417163969
Leonardo1596 <leonardoemanuel156@gmail.com>
2026-04-15 05:50:40 -0300

    removed date

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -73,7 +73,7 @@ export const createPendingTransaction = async (req, res) => {
       description,
       amount: numericAmount,
       source,
-      createdAt: date ? new Date(date) : new Date()
+      createdAt: new Date()
     });
 
     return res.status(201).json(pending);
